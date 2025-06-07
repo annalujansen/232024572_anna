0440c6a218aabecd2c2f9d34e0738c05a0388c28
Anna Luiza <annalu.nj@gmail.com>
2025-06-07 02:12:02 +0000

    [16/30] teste PASSA para vitoria de O na primeira coluna

--- src/velha.cpp
+++ src/velha.cpp
@@ -13,9 +13,8 @@ int verifica_jogo_da_velha(int tabuleiro[3][3]) {
             tabuleiro[2][1] == tabuleiro[2][2]) && tabuleiro[2][0] != 0)
             { return tabuleiro[2][0]; }  // Vitória na terceira linha
         if ((tabuleiro[0][0] == tabuleiro[1][0] &&
-            tabuleiro[1][0] == tabuleiro[2][0]) && tabuleiro[0][0]
-            && tabuleiro[0][0] == 1 && tabuleiro[1][0] != 0)
-            { return tabuleiro[0][0]; }  // Vitória na primeira coluna para X
+            tabuleiro[1][0] == tabuleiro[2][0]) && tabuleiro[0][0] != 0)
+            { return tabuleiro[0][0]; }  // Vitória na primeira coluna
     }
     return -1;  // Jogo indefinido
 }
