46ff247d092c9f1fd4fa41acc3aea2f039b22222
Anna Luiza <annalu.nj@gmail.com>
2025-06-07 02:20:44 +0000

    [22/30] teste PASSA para vitoria de X na terceira coluna

--- src/velha.cpp
+++ src/velha.cpp
@@ -18,6 +18,10 @@ int verifica_jogo_da_velha(int tabuleiro[3][3]) {
         if ((tabuleiro[0][1] == tabuleiro[1][1] &&
             tabuleiro[1][1] == tabuleiro[2][1]) && tabuleiro[0][1] != 0)
             { return tabuleiro[0][1]; }  // Vitória na segunda coluna
+        if ((tabuleiro[0][2] == tabuleiro[1][2] &&
+            tabuleiro[1][2] == tabuleiro[2][2]) && tabuleiro[0][2] == 1
+            && tabuleiro[0][2] != 0)
+            { return tabuleiro[0][2]; }  // Vitória na terceira coluna POR X
     }
     return -1;  // Jogo indefinido
 }
