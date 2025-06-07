144955a97623acdc02e076bf3b74a768003ab6f2
Anna Luiza <annalu.nj@gmail.com>
2025-06-07 00:50:09 +0000

    [07/30] teste FALHA para vitoria de O na primeira linha

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -42,3 +42,13 @@ TEST_CASE("Vitória na primeira linha para X") {
 
     REQUIRE(verifica_jogo_da_velha(tabuleiro) == 1);
 }
+
+TEST_CASE("Vitória na primeira linha para O") {
+    int tabuleiro[3][3] = {
+        {2, 2, 2},
+        {1, 0, 1},
+        {0, 0, 0}
+    };
+
+    REQUIRE(verifica_jogo_da_velha(tabuleiro) == 2);
+}
