dd176c81d596115beab01a8bc97757b925b38677
Anna Luiza <annalu.nj@gmail.com>
2025-06-07 01:01:11 +0000

    [08/30] teste PASSA para vitoria na primeira linha

--- velha.cpp
+++ velha.cpp
@@ -1,15 +1,14 @@
 // Copyright 2025 <Anna Luiza Jansen>
 #include "velha.hpp"
 
-int verifica_jogo_da_velha(int tabuleiro[3][3])
-{
-    for (int i=0; i<3; i++) {
-        if ((tabuleiro[1][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[1][2]) && tabuleiro[1][0] != 0) {
-            return tabuleiro[1][0]; // Vitória na segunda linha
-        }
-        if ((tabuleiro[0][0] == tabuleiro[0][1] && tabuleiro[0][1] == tabuleiro[0][2] && tabuleiro[0][0] == 1) && tabuleiro[0][0] != 0) {
-            return tabuleiro[0][0]; // Vitória na primeira linha para X
-        }
+int verifica_jogo_da_velha(int tabuleiro[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        if ((tabuleiro[1][0] == tabuleiro[1][1] &&
+            tabuleiro[1][1] == tabuleiro[1][2]) && tabuleiro[1][0] != 0)
+            { return tabuleiro[1][0]; }  // Vitória na segunda linha
+        if ((tabuleiro[0][0] == tabuleiro[0][1] &&
+            tabuleiro[0][1] == tabuleiro[0][2]) && tabuleiro[0][0] != 0)
+            { return tabuleiro[0][0]; }  // Vitória na primeira linha
     }
-    return -1; // Jogo indefinido
+    return -1;  // Jogo indefinido
 }
