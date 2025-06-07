323688de7486b2e5d418d8e4b6acf14c6d4940cd
Anna Luiza <annalu.nj@gmail.com>
2025-06-07 03:11:05 +0000

    teste PASSA para impossivel com dois vencedores

--- src/velha.cpp
+++ src/velha.cpp
@@ -3,6 +3,8 @@
 
 int verifica_jogo_da_velha(int tabuleiro[3][3]) {
     int countX = 0, countO = 0;
+    int vitoriaX = 0, vitoriaO = 0;
+
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (tabuleiro[i][j] == 1)
@@ -22,7 +24,8 @@ int verifica_jogo_da_velha(int tabuleiro[3][3]) {
         if (tabuleiro[i][0] == tabuleiro[i][1] &&
             tabuleiro[i][1] == tabuleiro[i][2] &&
             tabuleiro[i][0] != 0) {
-            return tabuleiro[i][0];
+            if (tabuleiro[i][0] == 1) vitoriaX++;
+            else vitoriaO++;
         }
     }
 
@@ -31,7 +34,8 @@ int verifica_jogo_da_velha(int tabuleiro[3][3]) {
         if (tabuleiro[0][i] == tabuleiro[1][i] &&
             tabuleiro[1][i] == tabuleiro[2][i] &&
             tabuleiro[0][i] != 0) {
-            return tabuleiro[0][i];
+            if (tabuleiro[0][i] == 1) vitoriaX++;
+            else vitoriaO++;
         }
     }
 
@@ -39,15 +43,25 @@ int verifica_jogo_da_velha(int tabuleiro[3][3]) {
     if (tabuleiro[0][0] == tabuleiro[1][1] &&
         tabuleiro[1][1] == tabuleiro[2][2] &&
         tabuleiro[0][0] != 0) {
-        return tabuleiro[0][0];
+        if (tabuleiro[0][0] == 1) vitoriaX++;
+        else vitoriaO++;
     }
 
     // Verifica vitória na diagonal secundária
     if (tabuleiro[0][2] == tabuleiro[1][1] &&
         tabuleiro[1][1] == tabuleiro[2][0] &&
         tabuleiro[0][2] != 0) {
-        return tabuleiro[0][2];
+        if (tabuleiro[0][2] == 1) vitoriaX++;
+        else vitoriaO++;
     }
+
+    // Detecta situação impossível: dois vencedores
+    if (vitoriaX > 0 && vitoriaO > 0) {
+        return -2;
+    }
+
+    if (vitoriaX > 0) return 1;
+    if (vitoriaO > 0) return 2;
 
     // Verifica empate
     bool cheio = true;
