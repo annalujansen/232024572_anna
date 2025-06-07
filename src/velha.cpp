8474c5d3fabdf22374428dcb024c37712263bd34
Anna Luiza <annalu.nj@gmail.com>
2025-06-07 02:45:19 +0000

    [34/30] teste PASSA para empate

--- src/velha.cpp
+++ src/velha.cpp
@@ -2,31 +2,52 @@
 #include "jogo/velha.hpp"
 
 int verifica_jogo_da_velha(int tabuleiro[3][3]) {
+    // Verifica linhas
     for (int i = 0; i < 3; i++) {
-        if ((tabuleiro[1][0] == tabuleiro[1][1] &&
-            tabuleiro[1][1] == tabuleiro[1][2]) && tabuleiro[1][0] != 0)
-            { return tabuleiro[1][0]; }  // Vitória na segunda linha
-        if ((tabuleiro[0][0] == tabuleiro[0][1] &&
-            tabuleiro[0][1] == tabuleiro[0][2]) && tabuleiro[0][0] != 0)
-            { return tabuleiro[0][0]; }  // Vitória na primeira linha
-        if ((tabuleiro[2][0] == tabuleiro[2][1] &&
-            tabuleiro[2][1] == tabuleiro[2][2]) && tabuleiro[2][0] != 0)
-            { return tabuleiro[2][0]; }  // Vitória na terceira linha
-        if ((tabuleiro[0][0] == tabuleiro[1][0] &&
-            tabuleiro[1][0] == tabuleiro[2][0]) && tabuleiro[0][0] != 0)
-            { return tabuleiro[0][0]; }  // Vitória na primeira coluna
-        if ((tabuleiro[0][1] == tabuleiro[1][1] &&
-            tabuleiro[1][1] == tabuleiro[2][1]) && tabuleiro[0][1] != 0)
-            { return tabuleiro[0][1]; }  // Vitória na segunda coluna
-        if ((tabuleiro[0][2] == tabuleiro[1][2] &&
-            tabuleiro[1][2] == tabuleiro[2][2]) && tabuleiro[0][2] != 0)
-            { return tabuleiro[0][2]; }  // Vitória na terceira coluna
-        if ((tabuleiro[0][0] == tabuleiro[1][1] &&
-            tabuleiro[1][1] == tabuleiro[2][2]) && tabuleiro[0][0] != 0)
-            { return tabuleiro[0][0]; }  // Vitória na diagonal principal
-        if ((tabuleiro[0][2] == tabuleiro[1][1] &&
-            tabuleiro[1][1] == tabuleiro[2][0]) && tabuleiro[0][2] != 0)
-            { return tabuleiro[0][2]; }  // Vitória na diagonal secundária
+        if (tabuleiro[i][0] == tabuleiro[i][1] &&
+            tabuleiro[i][1] == tabuleiro[i][2] &&
+            tabuleiro[i][0] != 0) {
+            return tabuleiro[i][0];
+        }
     }
-    return -1;  // Jogo indefinido
+
+    // Verifica colunas
+    for (int i = 0; i < 3; i++) {
+        if (tabuleiro[0][i] == tabuleiro[1][i] &&
+            tabuleiro[1][i] == tabuleiro[2][i] &&
+            tabuleiro[0][i] != 0) {
+            return tabuleiro[0][i];
+        }
+    }
+
+    // Verifica diagonal principal
+    if (tabuleiro[0][0] == tabuleiro[1][1] &&
+        tabuleiro[1][1] == tabuleiro[2][2] &&
+        tabuleiro[0][0] != 0) {
+        return tabuleiro[0][0];
+    }
+
+    // Verifica diagonal secundária
+    if (tabuleiro[0][2] == tabuleiro[1][1] &&
+        tabuleiro[1][1] == tabuleiro[2][0] &&
+        tabuleiro[0][2] != 0) {
+        return tabuleiro[0][2];
+    }
+
+    // Verifica empate (nenhuma posição igual a 0)
+    bool cheio = true;
+    for (int i = 0; i < 3 && cheio; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (tabuleiro[i][j] == 0) {
+                cheio = false;
+                break;
+            }
+        }
+    }
+
+    if (cheio) {
+        return 0; // Empate
+    }
+
+    return -1; // Jogo em andamento
 }
